3aaf20457025b87f5483bf68aa445ddb226cf33e
Darius Seals <tristarmediagroupinc@gmail.com>
2020-01-10 15:31:44 -0500

    Updates

---
+++ precache-manifest.8eb0c047f2dd0aaecfe17de91c3dd937.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo_buddies/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cc12306c94dd144ff181",
+    "url": "/Robo_buddies/static/js/runtime~main.e3155853.js"
+  },
+  {
+    "revision": "49c7e387cb84f1268814",
+    "url": "/Robo_buddies/static/js/main.96788fb9.chunk.js"
+  },
+  {
+    "revision": "d968eccbc445cca34540",
+    "url": "/Robo_buddies/static/js/2.5d3c1a95.chunk.js"
+  },
+  {
+    "revision": "49c7e387cb84f1268814",
+    "url": "/Robo_buddies/static/css/main.25b00a46.chunk.css"
+  },
+  {
+    "revision": "d968eccbc445cca34540",
+    "url": "/Robo_buddies/static/css/2.7aa789f3.chunk.css"
+  },
+  {
+    "revision": "cef1486bf781505b710c01af87461867",
+    "url": "/Robo_buddies/index.html"
+  }
+];
